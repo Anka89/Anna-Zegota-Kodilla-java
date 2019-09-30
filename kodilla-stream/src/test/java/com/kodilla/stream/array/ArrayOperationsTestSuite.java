53502f47e864450785d83a371ead1d59b6a35104
Ania <zegota.anka@gmail.com>
2019-09-30 13:00:22 +0200

    Task ArrayOperation added

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,19 @@
+package com.kodilla.stream.array;
+
+import array.ArrayOperations;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayOperationsTestSuite {
+    @Test
+    public void testGetAverage(){
+        //Given
+        int numbers [] = {1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6};
+        double averageExpected = 3.5;
+        //When
+        double average = ArrayOperations.getAverage(numbers);
+        //Then
+        System.out.println( (average) );
+        Assert.assertEquals(averageExpected, average, 0.001);
+    }
+}
