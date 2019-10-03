9ed989f4e2de74203cada2b4b0fc309177f1eb4a
Ania <zegota.anka@gmail.com>
2019-10-03 14:46:26 +0200

    FirstChallenge added

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,10 +1,24 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.test.FirstChallenge;
 
 public class ExceptionModuleRunner {
     public static void main(String args[]) {
-        FileReader fileReader = new FileReader();
-        fileReader.readFile();
+
+//        FileReader fileReader = new FileReader();
+//        fileReader.readFile();
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        double result= 0;
+
+        try {
+            result = firstChallenge.divide( 3, 0 );
+        } catch (ArithmeticException e) {
+            System.out.println( "You can`t divide by 0, please put `b` again" );
+        } finally {
+            System.out.println( result );
+        }
     }
 }
